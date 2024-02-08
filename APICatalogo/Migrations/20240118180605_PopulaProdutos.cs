9ccdc6dd2f797f6d6e5f6e312c21e33abb29d3d0
Viteira <jhonnvicttorr@gmail.com>
2024-02-08 12:00:22 -0300

    first commit

---
+++ APICatalogo/Migrations/20240118180605_PopulaProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Coca-Cola Diet','Refrigerante de cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Salgado de queijo','Salgado frito de queijo',3.99,'salgado.jpg',10,now(),2)");
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +
+                "Values('Pudim','Pudim de leite condensado 100g',7.75,'pudim.jpg',20,now(),3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
